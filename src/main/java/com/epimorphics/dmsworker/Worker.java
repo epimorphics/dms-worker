9dac70b859b5c9de9fa05eef51fed8031f1c77ca
der <dave@epimorphics.com>
2015-03-02 18:58:49 +0000

    Configure default test setup to run locally

--- src/main/java/com/epimorphics/dmsworker/Worker.java
+++ src/main/java/com/epimorphics/dmsworker/Worker.java
@@ -12,7 +12,7 @@ package com.epimorphics.dmsworker;
 import com.epimorphics.appbase.core.AppConfig;
 
 public class Worker {
-    public static final String CONFIGS="/opt/dms-worker/conf/app.conf,/etc/dms-worker/app.conf";
+    public static final String CONFIGS="/opt/dms-worker/conf/app.conf,/etc/dms-worker/app.conf,src/test/conf";
     
     public static void main(String[] args) {
         AppConfig.startApp("DMSWorker", CONFIGS);
